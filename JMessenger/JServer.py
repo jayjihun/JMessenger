44f0d43cf4a7659d078e6e30530243b40c647992
Jihun-Desktop <t080205@gmail.com>
2015-11-30 00:44:58 +0900

    Server Completed by Desktop

--- JMessenger/JServer.py
+++ JMessenger/JServer.py
@@ -7,81 +7,91 @@ from threading import Thread
 import re
 from ServerDB import ServerDB
 
-HOST = '175.198.72.181' #must be input parameter @TODO
+HOST = '175.198.72.181'
 PORT = 11557
 ADDR = (HOST,PORT)
 inputs = Queue()
 sendsock = socket(AF_INET,SOCK_STREAM)
 identifier = re.compile(r'[a-zA-Z0-9_]{4,12}')
-
-class Message(object):
-    def __init__(self,line,ip_from,wfile):
-        self.line = line
-        self.ip_from = ip_from
-        self.wfile=wfile
-        #self.wfile.write("Message()".encode())
-
-class WorkerThread(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.daemon = True
-        self.db = None
-
+db=ServerDB()
+    
+class ServerHandler(StreamRequestHandler):
     def jointry(self,line):
-        id = line.split(' ')[1]
-        pw = line.split(' ')[2]
+        try:
+            id = line.split(' ')[1]
+            pw = line.split(' ')[2]
+        except:
+            return 'JOINR FAIL INVAL'
         if identifier.fullmatch(id) is None:
             return 'JOINR FAIL INVALIDID'
         if identifier.fullmatch(pw) is None:
             return 'JOINR FAIL INVALIDPW'
-        if self.db.joinuser(id,pw):
+        if db.joinuser(id,pw):
             return 'JOINR SUCCESS'
         return 'JOINR FAIL DUPLICATE'
 
+    def login(self,line,add):
+        try:
+            id = line.split(' ')[1]
+            pw = line.split(' ')[2]
+        except:
+            return 'LOGINR FAIL INVAL'
+        if identifier.fullmatch(id) is None:
+            return 'LOGINR FAIL INVALIDID'
+        if identifier.fullmatch(pw) is None:
+            return 'LOGINR FAIL INVALIDPW'
+        return db.loginuser(id,pw,add)
 
-    def execute(self,line,ip_from):
+    def contry(self,line,add):
+        try:
+            otherid = line.split(' ')[1]
+        except:
+            return 'CONR FAIL INVAL'           
+        if identifier.fullmatch(otherid) is None:
+            return 'CONR FAIL INVALIDID'
+        otheradd = db.conuser(otherid)
+        if type(otheradd) == str:
+            return otheradd
+        return 'CONR SUCCESS %s %d'%otheradd
+
+    def logout(self,line):
+        try:
+            id = line.split(' ')[1]
+        except:
+            return 'LOGOUTR FAIL'
+        if identifier.fullmatch(id) is None:
+            return 'LGOUTR FAIL INVALIDID'
+        return db.logoutuser(id)
+
+    def process(self, line, add):
         try:
             code = line.split(' ')[0]
             if code == 'JOIN':
                 return self.jointry(line)
-        except:
+            elif code == 'LOGIN':
+                return self.login(line,add)
+            elif code == 'CON':
+                return self.contry(line,add)
+            elif code == 'LOGOUT':
+                return self.logout(line)
+        except Exception as e:
+            print(e.args)
             return 'NOT'
         return 'NOT'
 
-    def send(self,reply,wfile):
-        try:
-            wfile.write(bytes(reply+"\n",'utf-8'))
-        except Exception as e:
-            print("Send failed")
 
-    def run(self):
-        self.db = ServerDB()
-        print("Start Server")
-        while True:
-            message = inputs.get()
-            reply = self.execute(message.line,message.ip_from)
-            print(message.ip_from)
-            print('Reply :',reply)
-            self.send(reply,message.wfile)
-    
-class ServerHandler(StreamRequestHandler):
     def handle(self):
+        print("[REQUEST arrived]")
         add = self.client_address
         content = self.rfile.readline().strip().decode('utf-8')
-        message = Message(content,add,self.wfile)
-        sleep(2)
-        self.
-        self.wfile.write("Got it".encode())
-        inputs.put(message)
-        
         print("IP : %s, Message : %s"%(add,content))
-
-
+        reply = self.process(content, add)
+        print("Reply :",reply)
+        self.wfile.write(bytes(reply,'utf-8'))
 
 def main():
+    print("Start Server")
     tcpServ = TCPServer(ADDR, ServerHandler)
-    wthread = WorkerThread()
-    wthread.start()
     tcpServ.serve_forever();
 
 if __name__ == '__main__':
--- JMessenger/ServerDB.py
+++ JMessenger/ServerDB.py
@@ -4,12 +4,55 @@ class ServerDB(object):
     def __init__(self):
         self.con = sqlite3.connect(":memory:")
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS user(id text, pw text, ison int, primary key(id));")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS user(id text, pw text, ison int, ip text, port int, primary key(id));")
 
     def joinuser(self,id,pw):
         try:
-            self.cur.execute("INSERT INTO user VALUES(?,?,?);",(id,pw,0))
+            self.cur.execute("INSERT INTO user VALUES(?,?,?,?,?);",(id,pw,0,'',0))
             return True
         except Exception as e:
-            print(e.args)
             return False
+    def loginuser(self,id,pw,add):
+        try:
+            self.cur.execute("SELECT pw FROM user WHERE id=?;",(id,))
+            realpw = self.cur.fetchone()[0]
+        except:
+            return 'LOGINR FAIL NOSUCHID'
+        if realpw != pw:
+            return 'LOGINR FAIL WRONGPW'
+        
+        try:
+            self.cur.execute("UPDATE user SET ison=1, ip=?,port=? WHERE id=?",(add[0],add[1],id))
+            print("[LOGIN] ip : %s, port : %d, id : %s"%(add[0],add[1],id))
+            return 'LOGINR SUCCESS'
+        except:
+            return 'LOGINR FAIL UNKNOWN'
+
+    def conuser(self, id):
+        try:
+            self.cur.execute("SELECT ison, ip, port FROM user WHERE id=?;", (id,))
+            other = self.cur.fetchone()
+        except Exception as e:
+            print(e.args)
+            return 'CONR FAIL UNKNOWN'
+        if other is None:
+            return 'CONR FAIL NO_SUCH_USER'
+        if other[0] == 0:
+            return 'CONR FAIL USER_NOT_ONLINE'
+        return other[1:]
+
+    def logoutuser(self, id):
+        try:
+            self.cur.execute("SELECT ison FROM user WHERE id=?;",(id,))
+            ison = self.cur.fetchone()[0]
+        except Exception as e:
+            print(e.args)
+            return 'LOGOUTR FAIL'
+        if ison == 0:
+            return 'LOGOUTR FAIL'
+        try:
+            self.cur.execute("UPDATE user SET ison=0 WHERE id=?;",(id,))
+        except Exception as e:
+            print(e.args)
+            return 'LOGOUTR FAIL'
+        return 'LOGOUTR SUCCESS'

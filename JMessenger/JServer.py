e4f12cddb304038f38c415163507af20442b37a0
Jihun-Desktop <t080205@gmail.com>
2015-11-29 23:47:34 +0900

    using ServerHandler(StreamRequestHandler) and TCPServer

--- JMessenger/JServer.py
+++ JMessenger/JServer.py
@@ -1,4 +1,4 @@
-from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH)
+from socketserver import TCPServer, StreamRequestHandler
 from socket import *
 from time import ctime, sleep
 import sys
@@ -7,7 +7,7 @@ from threading import Thread
 import re
 from ServerDB import ServerDB
 
-HOST = '175.198.72.171'
+HOST = '175.198.72.181' #must be input parameter @TODO
 PORT = 11557
 ADDR = (HOST,PORT)
 inputs = Queue()
@@ -15,13 +15,16 @@ sendsock = socket(AF_INET,SOCK_STREAM)
 identifier = re.compile(r'[a-zA-Z0-9_]{4,12}')
 
 class Message(object):
-    def __init__(self,line,ip_from):
+    def __init__(self,line,ip_from,wfile):
         self.line = line
         self.ip_from = ip_from
+        self.wfile=wfile
+        #self.wfile.write("Message()".encode())
 
 class WorkerThread(Thread):
     def __init__(self):
         Thread.__init__(self)
+        self.daemon = True
         self.db = None
 
     def jointry(self,line):
@@ -39,38 +42,44 @@ class WorkerThread(Thread):
     def execute(self,line,ip_from):
         try:
             code = line.split(' ')[0]
-            print(code)
             if code == 'JOIN':
                 return self.jointry(line)
         except:
             return 'NOT'
-        return 'hi'
+        return 'NOT'
+
+    def send(self,reply,wfile):
+        try:
+            wfile.write(bytes(reply+"\n",'utf-8'))
+        except Exception as e:
+            print("Send failed")
 
     def run(self):
         self.db = ServerDB()
+        print("Start Server")
         while True:
-            while not inputs.empty():
-                message = inputs.get()
-                line = message.line
-                ip_from = message.ip_from
-                reply = self.execute(line,ip_from)
-                print('Reply :',reply)
-                sendsock.connect(ip_from)
-                sendsock.send(bytes(reply,'utf-8'))
-                sendsock.close()
+            message = inputs.get()
+            reply = self.execute(message.line,message.ip_from)
+            print(message.ip_from)
+            print('Reply :',reply)
+            self.send(reply,message.wfile)
     
-class ServerHandler(SRH):
+class ServerHandler(StreamRequestHandler):
     def handle(self):
         add = self.client_address
         content = self.rfile.readline().strip().decode('utf-8')
-        message = Message(content,add)
+        message = Message(content,add,self.wfile)
+        sleep(2)
+        self.
+        self.wfile.write("Got it".encode())
         inputs.put(message)
+        
         print("IP : %s, Message : %s"%(add,content))
 
 
 
 def main():
-    tcpServ = TCP(ADDR, ServerHandler)
+    tcpServ = TCPServer(ADDR, ServerHandler)
     wthread = WorkerThread()
     wthread.start()
     tcpServ.serve_forever();
